6ae18e57d1c910da7fd5a8043a01fbb993674e44
MuhamdShahab <muhammadshahab.business#gmail.com>
2023-03-08 14:37:38 +0500

    First Commit

---
+++ Hands on SKLEARN and TF/chapter-2.py
@@ -0,0 +1,12 @@
+#Chapter:2 End to End Machine Learning Project
+#Basic steps to follow
+#1. look at the big Picture
+#2. Get the data
+#3. Discover and visualize the data
+#4. Prepare the data for the Machine learning algorithms 
+#5. Select a model and train it
+#6. Fine-tune your model 
+#7. Present your solution
+#8. Launch, monitor and maintain your system
+
+
